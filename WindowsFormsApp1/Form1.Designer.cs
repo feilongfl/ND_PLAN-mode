65f5e1fae68017c78749df4297192c798ce4db93
feilong <feilongphone@gmail.com>
2019-04-17 10:35:40 +0800

    base gui

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelGS = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelAP = new System.Windows.Forms.Label();
+            this.labelDis = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelLDme = new System.Windows.Forms.Label();
+            this.labeRDme = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 11F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 11F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(116, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TAS";
+            // 
+            // labelGS
+            // 
+            this.labelGS.AutoSize = true;
+            this.labelGS.Font = new System.Drawing.Font("宋体", 14F);
+            this.labelGS.ForeColor = System.Drawing.Color.White;
+            this.labelGS.Location = new System.Drawing.Point(55, 16);
+            this.labelGS.Name = "labelGS";
+            this.labelGS.Size = new System.Drawing.Size(39, 19);
+            this.labelGS.TabIndex = 1;
+            this.labelGS.Text = "267";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(153, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "263";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(25, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "175  /  5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 14F);
+            this.label6.ForeColor = System.Drawing.Color.Violet;
+            this.label6.Location = new System.Drawing.Point(606, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 19);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "DEP-3";
+            // 
+            // labelAP
+            // 
+            this.labelAP.AutoSize = true;
+            this.labelAP.Font = new System.Drawing.Font("宋体", 14F);
+            this.labelAP.ForeColor = System.Drawing.Color.White;
+            this.labelAP.Location = new System.Drawing.Point(606, 45);
+            this.labelAP.Name = "labelAP";
+            this.labelAP.Size = new System.Drawing.Size(89, 19);
+            this.labelAP.TabIndex = 5;
+            this.labelAP.Text = "1141.03z";
+            // 
+            // labelDis
+            // 
+            this.labelDis.AutoSize = true;
+            this.labelDis.Font = new System.Drawing.Font("宋体", 14F);
+            this.labelDis.ForeColor = System.Drawing.Color.White;
+            this.labelDis.Location = new System.Drawing.Point(606, 64);
+            this.labelDis.Name = "labelDis";
+            this.labelDis.Size = new System.Drawing.Size(59, 19);
+            this.labelDis.TabIndex = 6;
+            this.labelDis.Text = "5.7NM";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 14F);
+            this.label9.ForeColor = System.Drawing.Color.Green;
+            this.label9.Location = new System.Drawing.Point(33, 623);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 19);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "VOR L";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("宋体", 14F);
+            this.label10.ForeColor = System.Drawing.Color.Green;
+            this.label10.Location = new System.Drawing.Point(33, 652);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 19);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "SSB";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("宋体", 11F);
+            this.label11.ForeColor = System.Drawing.Color.Green;
+            this.label11.Location = new System.Drawing.Point(33, 680);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 15);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "DME";
+            // 
+            // labelLDme
+            // 
+            this.labelLDme.AutoSize = true;
+            this.labelLDme.Font = new System.Drawing.Font("宋体", 14F);
+            this.labelLDme.ForeColor = System.Drawing.Color.Green;
+            this.labelLDme.Location = new System.Drawing.Point(70, 677);
+            this.labelLDme.Name = "labelLDme";
+            this.labelLDme.Size = new System.Drawing.Size(49, 19);
+            this.labelLDme.TabIndex = 10;
+            this.labelLDme.Text = "44.7";
+            // 
+            // labeRDme
+            // 
+            this.labeRDme.AutoSize = true;
+            this.labeRDme.Font = new System.Drawing.Font("宋体", 14F);
+            this.labeRDme.ForeColor = System.Drawing.Color.Green;
+            this.labeRDme.Location = new System.Drawing.Point(691, 677);
+            this.labeRDme.Name = "labeRDme";
+            this.labeRDme.Size = new System.Drawing.Size(49, 19);
+            this.labeRDme.TabIndex = 14;
+            this.labeRDme.Text = "44.7";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("宋体", 11F);
+            this.label14.ForeColor = System.Drawing.Color.Green;
+            this.label14.Location = new System.Drawing.Point(654, 680);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 15);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "DME";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("宋体", 14F);
+            this.label15.ForeColor = System.Drawing.Color.Green;
+            this.label15.Location = new System.Drawing.Point(654, 652);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(39, 19);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "RKN";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("宋体", 14F);
+            this.label16.ForeColor = System.Drawing.Color.Green;
+            this.label16.Location = new System.Drawing.Point(654, 623);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(59, 19);
+            this.label16.TabIndex = 11;
+            this.label16.Text = "VOR R";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(341, 113);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(47, 12);
+            this.label17.TabIndex = 15;
+            this.label17.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("宋体", 14F);
+            this.label18.ForeColor = System.Drawing.Color.Green;
+            this.label18.Location = new System.Drawing.Point(24, 343);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(19, 19);
+            this.label18.TabIndex = 16;
+            this.label18.Text = "W";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("宋体", 14F);
+            this.label19.ForeColor = System.Drawing.Color.Green;
+            this.label19.Location = new System.Drawing.Point(696, 343);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(19, 19);
+            this.label19.TabIndex = 17;
+            this.label19.Text = "E";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("宋体", 14F);
+            this.label20.ForeColor = System.Drawing.Color.Green;
+            this.label20.Location = new System.Drawing.Point(358, 34);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(48, 19);
+            this.label20.TabIndex = 18;
+            this.label20.Text = "N ↑";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("宋体", 14F);
+            this.label21.ForeColor = System.Drawing.Color.Green;
+            this.label21.Location = new System.Drawing.Point(369, 663);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(19, 19);
+            this.label21.TabIndex = 19;
+            this.label21.Text = "S";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(740, 705);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(740, 705);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.labeRDme);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.labelLDme);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelDis);
+            this.Controls.Add(this.labelAP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelGS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "ND - PLAN";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelGS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelAP;
+        private System.Windows.Forms.Label labelDis;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelLDme;
+        private System.Windows.Forms.Label labeRDme;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
